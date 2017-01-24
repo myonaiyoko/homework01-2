e1c39e5b230137e94198fc2df07b19660a0f49d3
Myonai <Myonai>
2017-01-24 22:50:12 +0900

    First Commit

---
+++ src/Eto.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+
+
+/**
+ * @author Administrator
+ *
+ */
+public class Eto {
+
+	public static String getEto(int year) {
+
+		if(year < 1) {
+			throw new IllegalArgumentException("紀元前は計算できません。");
+		}
+
+		List<String> etoList = new ArrayList<String>();
+		etoList.add("申");
+		etoList.add("酉");
+		etoList.add("戌");
+		etoList.add("亥");
+		etoList.add("子");
+		etoList.add("丑");
+		etoList.add("寅");
+		etoList.add("卯");
+		etoList.add("辰");
+		etoList.add("巳");
+		etoList.add("午");
+		etoList.add("未");
+
+		return etoList.get(year % 12);
+	}
+
+}
